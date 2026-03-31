1b0e7b08e2c258eb0174b6e845b59c42beacedcb
UchihaClanCoding <nimnethnetwork@gmail.com>
2026-03-31 10:24:32 +0530

    menu update

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ const mobileMenu = document.getElementById('mobile-menu');
 burger.addEventListener('click' , () => {
     burger.classList.toggle('open');
     mobileMenu.classList.toggle('open')
+});
+
+//when link click menu closed
+mobileMenu.querySelectorAll('a').forEach(link =>{
+    link.addEventListener('click', () => {
+        
+    })
 })
